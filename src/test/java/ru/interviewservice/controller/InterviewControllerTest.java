a0d329f1e0f632d7ce43c4e440a6b46730378094
Volod878 <volod1988@gmail.com>
2021-09-10 01:22:55 +0700

    added test

--- src/test/java/ru/interviewservice/controller/InterviewControllerTest.java
+++ src/test/java/ru/interviewservice/controller/InterviewControllerTest.java
@@ -8,21 +8,22 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.interviewservice.repository.InterviewRepository;
 
 import java.util.ArrayList;
 
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("Unit-level testing for InterviewControllerTest")
 public class InterviewControllerTest {
+
 
     @Autowired
     private MockMvc mockMvc;
@@ -33,12 +34,17 @@ public class InterviewControllerTest {
     @BeforeEach
     public void setup() {
         openMocks(this);
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new InterviewController(repository)).build();
     }
 
     @Test
     public void shouldReturnView() throws Exception {
-        given(repository.findAll()).willReturn(new ArrayList<>());
-        mockMvc.perform(get("/").contentType(MediaType.TEXT_HTML))
-                .andExpect(status().is2xxSuccessful());
+        when(repository.findAll()).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("list_interview"))
+                .andExpect(model().attributeExists("sorter", "filter", "items", "item"));
     }
 }
